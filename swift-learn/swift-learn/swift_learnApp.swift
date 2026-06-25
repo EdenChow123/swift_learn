a0f64ff626170d0e3d2393fc87b4c973db3da6cc
EdenChow <h2276252@qq.com>
2026-06-25 10:28:09 +0800

    Creat a new project .

---
+++ swift-learn/swift-learn/swift_learnApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_learnApp.swift
+//  swift-learn
+//
+//  Created by Eden on 2026/6/25.
+//
+
+import SwiftUI
+
+@main
+struct swift_learnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
